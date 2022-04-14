5c175c46d11dc6dd2752a4759d47f2f1019144ac
BesbesRiadh <besbes.med.riadh@gmail.com>
2022-04-15 00:57:17 +0200

    modifs

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -6,38 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegistrationControllerTest extends WebTestCase
 {
-    // public function testRegistrationSuccessfull(): void
-    // {
-    //     $client = static::createClient();
-    //     $crawler = $client->request('POST', '/register');
-    //     $token = $crawler->filter('[name="registration_form[_token]"]')->attr("value");
-    //     // dd($token);
-    //     $form = $crawler->selectButton('Register')->form([
-    //         "registration_form[email]" => "tester@gmail.fr",
-    //         "registration_form[plainPassword]" => "motdepasse",
-    //         "registration_form[_token]" => $token
-    //     ]);
+    /**
+     * @dataProvider dataProviderForTest
+     */
+    public function testRegistrationSuccessfull($email, $password): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', '/register');
+        $token = $crawler->filter('[name="registration_form[_token]"]')->attr("value");
+        $form = $crawler->selectButton('Register')->form([
+            "registration_form[email]" => $email,
+            "registration_form[plainPassword]" => $password,
+            "registration_form[_token]" => $token
+        ]);
 
-    //     $client->submit($form);
-    //     $this->assertResponseRedirects('/');
-    //     $client->followRedirect();
-    //     $this->assertResponseIsSuccessful();
-    // }
+        $client->submit($form);
+        $this->assertResponseRedirects('/');
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
 
     public function testRegistrationUnsuccessfull(): void
     {
         $client = static::createClient();
         $crawler = $client->request('POST', '/register');
         $token = $crawler->filter('[name="registration_form[_token]"]')->attr("value");
-        // dd($token);
+        // dd($client->getResponse());
         $form = $crawler->selectButton('Register')->form([
             "registration_form[email]" => "tester@gmail.fr",
             "registration_form[plainPassword]" => "motdepasse",
             "registration_form[_token]" => $token
         ]);
-
+        $response = $client->getResponse();
         $client->submit($form);
-        $this->assertResponseRedirects('/register');
-        $client->followRedirect();
+    }
+
+    public function dataProviderForTest()
+    {
+        return [
+            ['test1@gmail.com', 'motdepasse1'],
+            ['test2@gmail.com', 'motdepasse2'],
+            ['test3@gmail.com', 'motdepasse3']
+        ];
     }
 }
